9077d772c3ac38e1770755126d712bfee885b80c
PARTHIPAN M <101990386+mmmpartha@users.noreply.github.com>
2023-04-08 10:47:33 +0530

    Update userAction.js

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -64,7 +64,7 @@ export const register = (userData) => async (dispatch) => {
 
     const config = { headers: { "Content-Type": "multipart/form-data" } };
 
-    const { data } = await axios.post(`https://ecommerce-mern-boa3.onrender.com/api/v1/register`, userData, config);
+    const { data } = await axios.post(`/api/v1/register`, userData, config);
 
     dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
   } catch (error) {
