412b90cf5ca93fb993df373954f2f1a91d3e3824
PARTHIPAN M <101990386+mmmpartha@users.noreply.github.com>
2023-04-11 11:00:34 +0530

    Update userAction.js

--- src/actions/userAction.js
+++ src/actions/userAction.js
@@ -64,7 +64,7 @@ export const register = (userData) => async (dispatch) => {
 
     const config = { headers: { "Content-Type": "multipart/form-data" } };
 
-    const { data } = await axios.post(`/api/v1/register`, userData, config);
+    const { data } = await axios.post(`https://ecommerce-server-lp1j.onrender.com/api/v1/register`, userData, config);
 
     dispatch({ type: REGISTER_USER_SUCCESS, payload: data.user });
   } catch (error) {
@@ -80,7 +80,7 @@ export const loadUser = () => async (dispatch) => {
   try {
     dispatch({ type: LOAD_USER_REQUEST });
 
-    const { data } = await axios.get(`/api/v1/me`);
+    const { data } = await axios.get(`https://ecommerce-server-lp1j.onrender.com/api/v1/me`);
 
     dispatch({ type: LOAD_USER_SUCCESS, payload: data.user });
   } catch (error) {
